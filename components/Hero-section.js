9e366fb8ede6000ccff410ce9747fb3ad638296d
Julio Camargo <jcamargo@hotmail.co.uk>
2023-04-30 21:47:32 +0100

    768px complete

--- components/Hero-section.js
+++ components/Hero-section.js
@@ -3,19 +3,20 @@ import amy from '../public/assets/image-amy.webp'
 import styles from '@/styles/Hero.module.css'
 
 const HeroSection = () => {
-  return(
+  return (
     <div className={styles.heroSection}>
       <Image
-      src={amy}
-      alt="Amy"
-      width={300}
-      height={300}
+        src={amy}
+        alt="Amy"
+        width={300}
+        height={300}
       />
       <div className={styles.heroSection__Content}>
         <h1 className={styles.heroSection_H1}>Im Amy, and Id love to work on your next project</h1>
         <p className={styles.heroSection__P}>I love working with others to create beautiful design solutions. Ive designed everything from brand illustrations to complete mobile apps. Im also handy with a camera!</p>
-        </div>
         <button className={styles.heroSection__Button}>Free Consultation</button>
+      </div>
+    
     </div>
   )
 }
